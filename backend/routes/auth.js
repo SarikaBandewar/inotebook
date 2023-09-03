9eee6ce97b59db1b0df5ee81a42f61a50fc8a801
SarikaBandewar <52735009+SarikaBandewar@users.noreply.github.com>
2023-09-03 16:39:29 +0530

    added hashing and token

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const JWT_SECRET = 'dinkuMylove#';
 
 // Create a user using :POST "/api/auth/createUser"
 router.post(
@@ -30,15 +34,24 @@ router.post(
             'User with specified email found in db, please specify unique emailid',
         });
       }
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
+
       // user creation
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      console.log(authToken);
       // return success
-      res.json({ flag: 'successful' });
+      res.json({ flag: 'successful', token: authToken });
     } catch (error) {
       // log and return error
       console.log(error);
