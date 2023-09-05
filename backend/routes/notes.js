184d360b89dc83d9f8de9be33962356cd5cfec63
SarikaBandewar <52735009+SarikaBandewar@users.noreply.github.com>
2023-09-05 17:20:23 +0530

    added "Add a note" end point

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -2,11 +2,45 @@ const express = require('express');
 const fetchUser = require('../middleware/fetchuser');
 const router = express.Router();
 const Notes = require('../models/Notes');
+const { body, validationResult } = require('express-validator');
 
 // ROUTE 1 : get all notes using GET "/api/notes/fetchallnotes"
 router.get('/fetchallnotes', fetchUser, async (req, res) => {
   const notes = await Notes.find({ user: req.user.id });
   res.json(notes);
 });
+
+//ROUTE 2: add a note using POST "/api/notes/addnote" require login
+router.post(
+  '/addnote',
+  fetchUser,
+  [
+    body('title', 'Enter a valid title').isLength({ min: 3 }),
+    body('description', 'Enter valid description').isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.status(400).json({ errors: errors.array() });
+      }
+      const note = new Notes({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+
+      const savedNote = await note.save();
+
+      res.json(savedNote);
+    } catch (error) {
+      console.log(error);
+      res.status(501).send(error);
+    }
+  }
+);
 
 module.exports = router;
